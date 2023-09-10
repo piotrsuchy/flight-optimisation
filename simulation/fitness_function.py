501a4b6406ea389c90fa72d52ecc541990c6bef1
Piotr Suchy <piotrsuchy6@tlen.pl>
2023-09-10 23:21:23 +0200

    Fitness function file added

---
+++ simulation/fitness_function.py
@@ -0,0 +1,15 @@
+from simulation import Simulation
+
+
+def fitness_function():
+    pass
+
+
+def main():
+    sim = Simulation()
+    sim.run_simulation()
+    print(sim.airports_list)
+    print(sim.crew_information)
+
+
+main()
