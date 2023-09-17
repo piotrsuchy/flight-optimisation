eb3f588738a23ca5896ab8b52d139eb8c02f91f1
Piotr Suchy <piotrsuchy6@tlen.pl>
2023-09-17 21:47:03 +0200

    Event scheduler, flight and crew classes update

--- simulation/crew_member.py
+++ simulation/crew_member.py
@@ -4,32 +4,68 @@ days off,
 rest periods,
 flight duty period - pre-flight, post-flight etc.
 '''
+from event import Event
+from eventscheduler import EventScheduler
 
 class Pilot:
     _next_id = 1
 
-    def __init__(self):
+    def __init__(self, home_airport):
         self.id = Pilot._next_id
         Pilot._next_id += 1
-        self.free = True
-        self.home_airport = None
-        self.day_worked_hs = None
-        self.week_worked_hs = None
-        self.month_worked_hs = None
-        self.rest_period = None
+        self.is_available = True
+        self.home_airport = home_airport 
+        self.day_worked_hs = 0
+        self.week_worked_hs = 0
+        self.month_worked_hs = 0
 
     def is_free(self):
-        return self.free
+        return self.is_available
 
+    def occupy(self):
+        self.is_available = False
+
+    def release(self):
+        self.is_available = True
+
+    def start_rest(self, hours):
+        self.occupy()
+        # Scheduling an event for the end of rest
+        EventScheduler.schedule_event(hours, self.release)
+    
+    def flight_start(self, duration):
+        self.day_worked_hs += duration
+        self.week_worked_hs += duration
+        self.month_worked_hs += duration
 
 class FlightAttendant:
     _next_id = 1
     
-    def __init__(self):
+    def __init__(self, home_period):
         self.id = FlightAttendant._next_id
         FlightAttendant._next_id += 1
-        self.home_airport = None
-        self.day_worked_hs = None
-        self.week_worked_hs = None
-        self.month_worked_hs = None
-        self.rest_period = None
+        self.is_available = True
+        self.home_airport = home_period
+        self.day_worked_hs = 0
+        self.week_worked_hs = 0
+        self.month_worked_hs = 0
+        self.rest_period = 0
+
+    def is_free(self):
+        return self.is_available
+
+    def occupy(self):
+        self.is_available = False
+
+    def release(self):
+        self.is_available = True
+
+    def start_rest(self, hours):
+        self.occupy()
+        # Scheduling an event for the end of rest
+        EventScheduler.schedule_event(hours, self.release)
+
+    def flight_start(self, duration):
+        self.day_worked_hs += duration
+        self.week_worked_hs += duration
+        self.month_worked_hs += duration
--- simulation/flight.py
+++ simulation/flight.py
@@ -3,14 +3,17 @@ import math
 class Flight:
     _next_id = 1
 
-    def __init__(self, base_airport, destination_airport, plane):
+    def __init__(self, base_airport, destination_airport, plane, pilot, crew):
         self.id = Flight._next_id
         Flight._next_id += 1
         self.base_airport = base_airport
         self.destination_airport = destination_airport
         self.plane = plane
+        self.pilots = pilot
+        self.crew = crew
         self.distance = self.calculate_distance()
         self.duration = self.calculate_duration()
+        self.status = "started"
 
     def calculate_distance(self):
         return math.sqrt((self.base_airport.x - self.destination_airport.x)**2 +
@@ -18,3 +21,27 @@ class Flight:
 
     def calculate_duration(self):
         return self.distance / self.plane.speed
+
+    def start_flight(self):
+        if not all(pilot.is_available for pilot in self.pilots) or not all(attendant.is_available for attendant in self.crew):
+            print("Not all crew members or pilots are available!")
+            return
+
+        for pilot in self.pilots:
+            pilot.flight_start(self.duration)
+            pilot.occupy()
+        
+        for attendant in self.crew:
+            attendant.flight_start(self.duration)
+            attendant.occupy()
+
+        print(f"Flight {self.id} has started! Duration: {self.duration}")
+
+    def end_flight(self):
+        self.status = "completed"
+
+        for pilot in self.pilots:
+            pilot.start_rest(min(12, self.duration))
+
+        for attendant in self.crew:
+            attendant.start_rest(min(12, self.duration))
