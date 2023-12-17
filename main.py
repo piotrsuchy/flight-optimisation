a5a997a78061107dd21f9268feb407c3d7858308
Piotr Suchy <piotrsuchy6@tlen.pl>
2023-12-17 13:24:12 +0100

    Refactoring for main.py

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
-import time
 import json
 from src.allowed_approach.structures import Structures
 from src.allowed_approach.evol_algo import EvolutionaryAlgorithm
-from src.decorators import timing_decorator
-from debug.test_availability import test_availability
 from config.logs_and_args import get_args, setup_logging
-# from memory_profiler import profile
-
-# @timing_decorator
-# @profile
-
 
 def main():
     with open('parameters.json') as parameters_file:
         config = json.load(parameters_file)
 
     args = get_args()
-    # print(f"ARGS: {args.log}")
     setup_logging(args.log)
 
     initial_structures = Structures()
@@ -35,7 +26,7 @@ def main():
 
     # mutation
     evol_algo.evol_algo_loop_two_pop(config['algo']['N_ITERATIONS'])
-    evol_algo.print_cancellation_reason()
+    evol_algo.print_all_info()
 
     # test_availability(evol_algo)
 
