43e16ddcf606737a1a81e8a1f2e61847c690eabc
Piotr Suchy <piotrsuchy6@tlen.pl>
2023-12-19 16:36:44 +0100

    Added penalty and best_score printing, fixed initial_gen_with_updates, QoL fixes

--- unallowed_main.py
+++ unallowed_main.py
@@ -1,4 +1,5 @@
 import json
+import random
 from src.unallowed_approach.imp_evol_algo import ImpossibleEvolutionaryAlgorithm
 
 def main():
@@ -11,12 +12,16 @@ def main():
     # initial assignments - made only once
     imp_evol_algo.fill_in_distance_matrix()
     imp_evol_algo.assign_airports_to_crew_members()
+    print(f"Imp_evol_algo.pilots_status_pop: {imp_evol_algo.pilots_status_pop}")
+    print(f"Imp_evol_algo.attend_status_pop: {imp_evol_algo.attend_status_pop}")
     imp_evol_algo.create_initial_sols()
     imp_evol_algo.generate_training_hours()
 
-    imp_evol_algo.create_initial_generation()
+    imp_evol_algo.create_initial_generation_with_update()
     # imp_evol_algo.print_population()
+
     imp_evol_algo.update_fitness_for_all_sols()
+    initial_penalties = imp_evol_algo.get_penalties_for_sols()
     for i in range(len(imp_evol_algo.population)):
         print(f"START of first gen----")
         imp_evol_algo.print_penalties_for_sols(0, i)
@@ -25,9 +30,16 @@ def main():
     imp_evol_algo.print_fitness_scores(0)
     fit_scores = imp_evol_algo.get_fitness_scores()
 
-    imp_evol_algo.evolutionary_algorithm_loop(config['algo']['N_ITERATIONS'])
+    imp_evol_algo.evolutionary_algorithm_loop(config['algo']['N_ITERATIONS'],print_flag=0)
     print(f"Initial fitness values: {fit_scores}")
 
+    print(f"---Final penalties applied---")
+    for i in range(len(imp_evol_algo.population)):
+        imp_evol_algo.print_penalties_for_sols(config['algo']['N_ITERATIONS']+1, i)
+
+    print(f"---Initial penalties applied---")
+    for i in range(len(initial_penalties)):
+        print(f"Sol: {i}, Fit: {fit_scores[i]} Loc: {initial_penalties[i][0]}, Rest: {initial_penalties[i][1]}, Canc: {initial_penalties[i][2]}, train_ov: {initial_penalties[i][3]}, Prop. alloc: {initial_penalties[i][4]}")
 
 if __name__ == "__main__":
     main()
