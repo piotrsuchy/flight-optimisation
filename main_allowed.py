b92e439ef1151225c9f192f2006130ccebc1d039
Piotr Suchy <piotrsuchy6@tlen.pl>
2023-12-22 10:23:00 +0100

    Allowed approach - 2 working heuristics, saved params

--- main_allowed.py
+++ main_allowed.py
@@ -25,10 +25,11 @@ def main():
     evol_algo.print_fitness_scores(0)
 
     '''evolutionary algorithm loop with initializing new populations
-    at each loop and mutating the best solutions - doesn't work the best'''
-    # evol_algo.evol_algo_loop_with_init(config['algo']['N_ITERATIONS'])
+    at each loop and keeping best 50% of solutions, mutating whole pop
+    and keeping a copy of elite pop - best 25% to save best sols'''
+    evol_algo.evol_algo_loop_with_init(config['algo']['N_ITERATIONS'])
     '''Second option - taking the best solutions from '''
-    evol_algo.evol_algo_loop_two_pop(config['algo']['N_ITERATIONS'])
+    # evol_algo.evol_algo_loop_two_pop(config['algo']['N_ITERATIONS'])
     print(f"----------------------------------------------------------------")
     evol_algo.print_all_info()
 
