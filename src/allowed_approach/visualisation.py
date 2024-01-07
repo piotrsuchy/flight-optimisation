06f76078814413b8a5911c177c7e53c8233e4787
Piotr Suchy <piotrsuchy6@tlen.pl>
2024-01-07 22:04:51 +0100

    Visualisation for multiple plots added

--- src/allowed_approach/visualisation.py
+++ src/allowed_approach/visualisation.py
@@ -1,26 +1,83 @@
 import matplotlib.pyplot as plt
 import json
 
+def plot_compare_two_fitness_scores(file_path_1, file_path_2):
+    with open(f'{file_path_1}_scores.json', 'r') as file:
+        data_1 = json.load(file)
+
+    with open(f'{file_path_2}_scores.json', 'r') as file:
+        data_2 = json.load(file)
+
+    # extract data for plotting
+    iterations = [item['iteration'] for item in data_1['fitness_scores']]
+    best_scores = [item['best_score'] for item in data_1['fitness_scores']]
+    median_scores = [item['median_score'] for item in data_1['fitness_scores']]
+    top_half_medians = [item['top_half_median'] for item in data_1['fitness_scores']]
+    bottom_half_medians = [item['bottom_half_median'] for item in data_1['fitness_scores']]
+    
+    plt.figure(figsize=(10, 6))
+    plt.plot(iterations, best_scores, label='best score')
+    plt.plot(iterations, median_scores, label='median score')
+    plt.plot(iterations, top_half_medians, label='top half median')
+    plt.plot(iterations, bottom_half_medians, label='bottom half median')
+
+    iterations = [item['iteration'] for item in data_2['fitness_scores']]
+    best_scores = [item['best_score'] for item in data_2['fitness_scores']]
+    median_scores = [item['median_score'] for item in data_2['fitness_scores']]
+    top_half_medians = [item['top_half_median'] for item in data_2['fitness_scores']]
+    bottom_half_medians = [item['bottom_half_median'] for item in data_2['fitness_scores']]
+
+    plt.plot(iterations, best_scores, label='best score')
+    plt.plot(iterations, median_scores, label='median score')
+    plt.plot(iterations, top_half_medians, label='top half median')
+    plt.plot(iterations, bottom_half_medians, label='bottom half median')
+
+    plt.xlabel('iteration')
+    plt.ylabel('fitness score')
+    plt.title('Comparison of two fitness scores')
+    plt.legend()
+    plt.show()
+
+def plot_compare_multiple_fitness_scores(**kwargs):
+    plt.figure(figsize=(10, 6))
+
+    for file_path, label in kwargs.items():
+        with open(f'{file_path}_scores.json', 'r') as file:
+            data = json.load(file)
+
+        iterations = [item['iteration'] for item in data['fitness_scores']]
+        best_scores = [item['best_score'] for item in data['fitness_scores']]
+        median_scores = [item['median_score'] for item in data['fitness_scores']]
+
+        plt.plot(iterations, best_scores, label=f'best score - ({label})')
+        plt.plot(iterations, median_scores, label=f'median score - ({label})')
+
+    plt.xlabel('Iteration')
+    plt.ylabel('Fitness Score')
+    plt.title('Comparison of Fitness Scores for different operators')
+    plt.legend()
+    plt.show()
+
 def plot_fitness_scores(file_path):
     with open(f'{file_path}_scores.json', 'r') as file:
         data = json.load(file)
 
-    # Extract data for plotting
+    # extract data for plotting
     iterations = [item['iteration'] for item in data['fitness_scores']]
     best_scores = [item['best_score'] for item in data['fitness_scores']]
     median_scores = [item['median_score'] for item in data['fitness_scores']]
     top_half_medians = [item['top_half_median'] for item in data['fitness_scores']]
     bottom_half_medians = [item['bottom_half_median'] for item in data['fitness_scores']]
 
-    # Plotting
+    # plotting
     plt.figure(figsize=(10, 6))
-    plt.plot(iterations, best_scores, label='Best Score')
-    plt.plot(iterations, median_scores, label='Median Score')
-    plt.plot(iterations, top_half_medians, label='Top Half Median')
-    plt.plot(iterations, bottom_half_medians, label='Bottom Half Median')
-    plt.xlabel('Iteration')
-    plt.ylabel('Fitness Score')
-    plt.title('Evolution of Fitness Scores Over Iterations')
+    plt.plot(iterations, best_scores, label='best score')
+    plt.plot(iterations, median_scores, label='median score')
+    plt.plot(iterations, top_half_medians, label='top half median')
+    plt.plot(iterations, bottom_half_medians, label='bottom half median')
+    plt.xlabel('iteration')
+    plt.ylabel('fitness score')
+    plt.title('evolution of fitness scores over iterations')
     plt.legend()
     plt.show()
 
@@ -38,15 +95,15 @@ def plot_penalties(file_path):
 
     #plot penalties
     plt.figure(figsize=(10, 6))
-    plt.plot(iterations, cancelled_nums, label='Number of cancelled flights')
-    plt.plot(iterations, training_nums, label='Number of training overlaps')
-    plt.plot(iterations, dayoff_nums, label='Number of dayoff overlaps')
-    plt.plot(iterations, location_nums, label='Number of location penalties applied')
-    plt.plot(iterations, rest_nums, label='Number of rest penalties applied')
-    plt.plot(iterations, overwork_nums, label='Number of overwork hours')
+    plt.plot(iterations, cancelled_nums, label='number of cancelled flights')
+    plt.plot(iterations, training_nums, label='number of training overlaps')
+    plt.plot(iterations, dayoff_nums, label='number of dayoff overlaps')
+    plt.plot(iterations, location_nums, label='number of location penalties applied')
+    plt.plot(iterations, rest_nums, label='number of rest penalties applied')
+    plt.plot(iterations, overwork_nums, label='number of overwork hours')
     plt.yscale('log')
-    plt.xlabel('Iteration')
-    plt.ylabel('Number of penalties applied in the best solution')
-    plt.title('Evolution of number of different penalties applied on the best solution')
+    plt.xlabel('iteration')
+    plt.ylabel('number of penalties applied in the best solution')
+    plt.title('evolution of number of different penalties applied on the best solution')
     plt.legend()
     plt.show()
