0ee6fca7484f4ec196ff386dc3c6c1fe288a651a
Piotr Suchy <piotrsuchy6@tlen.pl>
2024-01-11 22:53:21 +0100

    Added timestamp to file names

--- main_tests.py
+++ main_tests.py
@@ -1,60 +1,76 @@
 import json
 import subprocess
+import datetime
 
-# FOR CHAT: test cases to write correctly!
 test_cases = [
+    # {
+    #     "algo": {
+    #         "POPULATION_SIZE": 20,
+    #         "N_ITERATIONS_AL": 40,
+    #         "ALLOWED_HEURISTIC": "work_time",
+    #         "ALLOWED_GEN_CREATION": "two_pop",
+    #         "MUTATION_RATE": 1
+    #     },
+    # },
+    # {
+    #     "algo": {
+    #         "POPULATION_SIZE": 20,
+    #         "N_ITERATIONS_AL": 40,
+    #         "ALLOWED_HEURISTIC": "random",
+    #         "ALLOWED_GEN_CREATION": "two_pop"
+    #     }
+    # },
+    # {
+    #     "algo": {
+    #         "POPULATION_SIZE": 20,
+    #         "N_ITERATIONS_AL": 40,
+    #         "ALLOWED_HEURISTIC": "smallest_id",
+    #         "ALLOWED_GEN_CREATION": "two_pop"
+    #     }
+    # },
     {
         "algo": {
             "POPULATION_SIZE": 20,
             "N_ITERATIONS_AL": 40,
             "ALLOWED_HEURISTIC": "work_time",
-            "ALLOWED_GEN_CREATION": "two_pop",
-            "MUTATION_RATE": 1
-        },
-        "algo": {
-            "POPULATION_SIZE": 20,
-            "N_ITERATIONS_AL": 40,
-            "ALLOWED_HEURISTIC": "random",
-            "ALLOWED_GEN_CREATION": "two_pop"
-        },
-        "algo": {
-            "POPULATION_SIZE": 20,
-            "N_ITERATIONS_AL": 40,
-            "ALLOWED_HEURISTIC": "smallest_id",
-            "ALLOWED_GEN_CREATION": "two_pop"
-        },
-        "algo": {
-            "POPULATION_SIZE": 20,
-            "N_ITERATIONS_AL": 40,
-            "ALLOWED_HEURISTIC": "work_time",
             "ALLOWED_GEN_CREATION": "with_init"
-        },
+        }
+    },
+    {
         "algo": {
             "POPULATION_SIZE": 20,
             "N_ITERATIONS_AL": 40,
             "ALLOWED_HEURISTIC": "random",
             "ALLOWED_GEN_CREATION": "with_init"
-        },
+        }
+    },
+    {
         "algo": {
             "POPULATION_SIZE": 20,
             "N_ITERATIONS_AL": 40,
             "ALLOWED_HEURISTIC": "smallest_id",
             "ALLOWED_GEN_CREATION": "with_init"
-        },
+        }
+    },
+    {
         "algo": {
             "POPULATION_SIZE": 20,
             "N_ITERATIONS_AL": 40,
             "ALLOWED_HEURISTIC": "work_time",
             "ALLOWED_GEN_CREATION": "with_init",
             "MUTATION_RATE": 0.25
-        },
+        }
+    },
+    {
         "algo": {
             "POPULATION_SIZE": 20,
             "N_ITERATIONS_AL": 40,
             "ALLOWED_HEURISTIC": "work_time",
             "ALLOWED_GEN_CREATION": "with_init",
             "MUTATION_RATE": 0.5
-        },
+        }
+    },
+    {
         "algo": {
             "POPULATION_SIZE": 20,
             "N_ITERATIONS_AL": 40,
@@ -66,6 +82,9 @@ test_cases = [
 ]
 
 structs_files = ['structs_I.json', 'structs_II.json', 'structs_III.json']
+
+def get_current_timestamp():
+    return datetime.datetime.now().strftime("%m-%d-%H-%M-%S")
 
 def update_parameters_file(params, file_path='parameters.json'):
     def update_recursive(d, u):
@@ -85,18 +104,21 @@ def update_parameters_file(params, file_path='parameters.json'):
         json.dump(updated_data, file, indent=4)
 
 def run_main_allowed(structs_file, plot_filename):
+    current_timestamp = get_current_timestamp()
     command = f'python main_allowed.py --pickle json'
-    inputs = f'{plot_filename}\n{structs_file}\n'
+    inputs = f'{plot_filename}_{current_timestamp}\n{structs_file}\n'
     process = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE)
     process.communicate(input=inputs.encode())
     return process.returncode == 0
 
 def main():
     for test_case_index, test_case in enumerate(test_cases):
+
+        print(f"Test case being run: {test_case}")
         update_parameters_file(test_case)
 
         for structs_file in structs_files:
-            plot_filename = f"plot_test_case_{test_case_index}_structs_{structs_file.split('.')[0]}.png"
+            plot_filename = f"plot_test_case_{test_case_index}_{structs_file.split('.')[0]}"
 
             print(f"Running test case {test_case_index} with {structs_file}")
             success = run_main_allowed(structs_file, plot_filename)
